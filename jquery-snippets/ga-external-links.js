c2dad69757d54ab513b16f33b314a626214eb1fd
Vladimir Komarov <jobs@vlkomarov.info>
2011-11-06 12:22:52 +0200

    Add jQuery 1.7 new code

--- jquery-snippets/ga-external-links.js
+++ jquery-snippets/ga-external-links.js
@@ -2,3 +2,9 @@ $( "a[class~='external']" ).bind( "click", function() {
 	var url = '/outgoing/'+ $( this ).attr( "href" );
 	_gaq.push( [ "_trackPageview", url ] );
 });
+
+// jQuery 1.7 `on` instead of `bind`
+$( "a[class~='external']" ).on( "click", function() {
+	var url = '/outgoing/'+ $( this ).attr( "href" );
+	_gaq.push( [ "_trackPageview", url ] );
+});
